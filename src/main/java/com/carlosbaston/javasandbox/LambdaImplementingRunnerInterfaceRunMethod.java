c84eeeee25f42134d77fc9b37e5ae37b6aff33a0
Carlos Baston <carlosbaston82@gmail.com>
2020-05-22 20:47:25 -0700

    Lambda Expression implementing Runner Interface run method

---
+++ src/main/java/com/carlosbaston/javasandbox/LambdaImplementingRunnerInterfaceRunMethod.java
@@ -0,0 +1,20 @@
+package com.carlosbaston.javasandbox;
+
+public class LambdaImplementingRunnerInterfaceRunMethod {
+
+    public static void main(String[] args) {
+
+        Runnable r = () -> {
+            for (int i = 0; i < 4; i++) {
+                System.out.println("Child Thread");
+            }
+        };
+
+
+        Thread t = new Thread(r);
+        t.start();
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Main Thread");
+        }
+    }
+}
