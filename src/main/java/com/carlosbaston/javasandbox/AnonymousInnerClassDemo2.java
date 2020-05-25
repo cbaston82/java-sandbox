bc9e2427de2b88145c3be4a3050324f0c6883d26
Carlos Baston <carlosbaston82@gmail.com>
2020-05-24 19:08:17 -0700

    Anonymous Inner Class Demo2

---
+++ src/main/java/com/carlosbaston/javasandbox/AnonymousInnerClassDemo2.java
@@ -0,0 +1,34 @@
+package com.carlosbaston.javasandbox;
+
+import com.carlosbaston.javasandbox.common.InterfaceThreeMethod;
+
+/**
+ * Anonymous Inner Class that implements an interface which contains multiple
+ * methods. Here Lambda cannot be used.
+ */
+class AnonymousInnerClassDemo2 {
+    public static void main(String[] args) {
+
+        InterfaceThreeMethod t = new InterfaceThreeMethod() {
+            @Override
+            public void method1() {
+                System.out.println("test1");
+            }
+
+            @Override
+            public void method2() {
+                System.out.println("test2");
+            }
+
+            @Override
+            public void method3() {
+                System.out.println("test3");
+            }
+        };
+
+        t.method1();
+        t.method2();
+        t.method3();
+    }
+}
+
